ba0d14ac2517cbec4140b4d88fb9c6b338623302
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-03-26 01:17:20 +1100

    fix: no longer gets registered

--- src/main/kotlin/dev/frozenmilk/dairy/core/dependencyresolution/dependencies/FeatureFalse.kt
+++ src/main/kotlin/dev/frozenmilk/dairy/core/dependencyresolution/dependencies/FeatureFalse.kt
@@ -7,5 +7,5 @@ import dev.frozenmilk.dairy.core.dependencyresolution.dependencyset.DependencySe
  * a dummy feature that is returned instead of a nullable outcome, this should never actually be returned to the consumer, as this should only be returned alongside a failed resolution, which then throws an error
  */
 internal object FeatureFalse : Feature {
-	override val dependencies: Set<Dependency<*, *>> = DependencySet(this).yields()
+	override val dependencies: Set<Dependency<*, *>> = DependencySet(this)
 }
