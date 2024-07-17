ddc92d9be2e401a6bd8ea41cc14c034c7a840883
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-07-17 11:33:48 +1000

    doc: Branch + BinaryBranch
    documentation improved, additionally, all force eval methods added to
    Branch to match BinaryBranch

--- src/main/kotlin/dev/frozenmilk/dairy/core/util/controller/calculation/logical/BinaryBranch.kt
+++ src/main/kotlin/dev/frozenmilk/dairy/core/util/controller/calculation/logical/BinaryBranch.kt
@@ -5,6 +5,13 @@ import dev.frozenmilk.dairy.core.util.controller.calculation.ControllerComponent
 import java.util.function.Function
 
 open class BinaryBranch<T>(val cond: ControllerComponent<T, Boolean>) {
+	/**
+	 * if [cond]:
+	 *
+	 * true -> [evalTrue]
+	 *
+	 * false -> [evalTrue]
+	 */
 	fun eval(evalTrue: ControllerCalculation<T>, evalFalse: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
 		return@ControllerCalculation if(cond.evaluate(accumulation, currentState, target, error, deltaTime)) {
 			evalTrue.evaluate(accumulation, currentState, target, error, deltaTime)
@@ -13,6 +20,17 @@ open class BinaryBranch<T>(val cond: ControllerComponent<T, Boolean>) {
 			evalFalse.evaluate(accumulation, currentState, target, error, deltaTime)
 		}
 	}
+	/**
+	 * if [cond]:
+	 *
+	 * true -> [evalTrue]
+	 *
+	 * false -> [evalTrue]
+	 *
+	 * evaluates both [evalTrue] and [evalFalse], regardless of which is used.
+	 * this is useful for when a calculation is cheap, but may cause issues if it has a big gap in evaluation,
+	 * e.g. D terms where previous error and delta time may be differently timestamped
+	 */
 	fun forceEval(evalTrue: ControllerCalculation<T>, evalFalse: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
 		return@ControllerCalculation if(cond.evaluate(accumulation, currentState, target, error, deltaTime)) {
 			evalFalse.evaluate(accumulation, currentState, target, error, deltaTime)
@@ -23,6 +41,13 @@ open class BinaryBranch<T>(val cond: ControllerComponent<T, Boolean>) {
 			evalFalse.evaluate(accumulation, currentState, target, error, deltaTime)
 		}
 	}
+	/**
+	 * if [cond]:
+	 *
+	 * true -> [evalTrue]
+	 *
+	 * false -> accumulation (no-op)
+	 */
 	fun evalTrue(evalTrue: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
 		if (cond.evaluate(accumulation, currentState, target, error, deltaTime)) {
 			evalTrue.evaluate(accumulation, currentState, target, error, deltaTime)
@@ -31,6 +56,17 @@ open class BinaryBranch<T>(val cond: ControllerComponent<T, Boolean>) {
 			accumulation
 		}
 	}
+	/**
+	 * if [cond]:
+	 *
+	 * true -> [evalTrue]
+	 *
+	 * false -> accumulation (no-op)
+	 *
+	 * evaluates [evalTrue], regardless of if it is used.
+	 * this is useful for when a calculation is cheap, but may cause issues if it has a big gap in evaluation,
+	 * e.g. D terms where previous error and delta time may be differently timestamped
+	 */
 	fun forceEvalTrue(evalTrue: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
 		return@ControllerCalculation if (cond.evaluate(accumulation, currentState, target, error, deltaTime)) {
 			evalTrue.evaluate(accumulation, currentState, target, error, deltaTime)
@@ -39,7 +75,13 @@ open class BinaryBranch<T>(val cond: ControllerComponent<T, Boolean>) {
 			evalTrue.evaluate(accumulation, currentState, target, error, deltaTime)
 			accumulation
 		}
-	}
+	}	/**
+	 * if [cond]:
+	 *
+	 * true -> accumulation (no-op)
+	 *
+	 * false -> [evalFalse]
+	 */
 	fun evalFalse(evalFalse: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
 		return@ControllerCalculation if (cond.evaluate(accumulation, currentState, target, error, deltaTime)) {
 			accumulation
@@ -48,6 +90,17 @@ open class BinaryBranch<T>(val cond: ControllerComponent<T, Boolean>) {
 			evalFalse.evaluate(accumulation, currentState, target, error, deltaTime)
 		}
 	}
+	/**
+	 * if [cond]:
+	 *
+	 * true -> accumulation (no-op)
+	 *
+	 * false -> [evalFalse]
+	 *
+	 * evaluates [evalFalse], regardless of if it is used.
+	 * this is useful for when a calculation is cheap, but may cause issues if it has a big gap in evaluation,
+	 * e.g. D terms where previous error and delta time may be differently timestamped
+	 */
 	fun forceEvalFalse(evalFalse: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
 		return@ControllerCalculation if (cond.evaluate(accumulation, currentState, target, error, deltaTime)) {
 			evalFalse.evaluate(accumulation, currentState, target, error, deltaTime)
@@ -58,9 +111,24 @@ open class BinaryBranch<T>(val cond: ControllerComponent<T, Boolean>) {
 		}
 	}
 
+	/**
+	 * simplified [BinaryBranch]
+	 */
 	class Accumulation<T>(cond: Function<T, Boolean>) : BinaryBranch<T>(ControllerComponent { accumulation, _, _, _, _ -> cond.apply(accumulation) })
+	/**
+	 * simplified [BinaryBranch]
+	 */
 	class State<T>(cond: Function<T, Boolean>) : BinaryBranch<T>(ControllerComponent { _, currentState, _, _, _ -> cond.apply(currentState) })
+	/**
+	 * simplified [BinaryBranch]
+	 */
 	class Target<T>(cond: Function<T, Boolean>) : BinaryBranch<T>(ControllerComponent { _, _, target, _, _ -> cond.apply(target) })
+	/**
+	 * simplified [BinaryBranch]
+	 */
 	class Error<T>(cond: Function<T, Boolean>) : BinaryBranch<T>(ControllerComponent { _, _, _, error, _ -> cond.apply(error) })
+	/**
+	 * simplified [BinaryBranch]
+	 */
 	class DeltaTime<T>(cond: Function<Double, Boolean>) : BinaryBranch<T>(ControllerComponent { _, _, _, _, deltaTime -> cond.apply(deltaTime) })
 }
--- src/main/kotlin/dev/frozenmilk/dairy/core/util/controller/calculation/logical/Branch.kt
+++ src/main/kotlin/dev/frozenmilk/dairy/core/util/controller/calculation/logical/Branch.kt
@@ -5,13 +5,76 @@ import dev.frozenmilk.dairy.core.util.controller.calculation.ControllerComponent
 import java.util.function.Function
 
 open class Branch<T, R>(val cond: ControllerComponent<T, R>) {
+	/**
+	 * matches [cond] -> [ControllerCalculation] via [map] or returns accumulation (no-op)
+	 */
 	fun map(map: Map<R, ControllerCalculation<T>>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
 		return@ControllerCalculation map[cond.evaluate(accumulation, currentState, target, error, deltaTime)]?.evaluate(accumulation, currentState, target, error, deltaTime) ?: accumulation
 	}
 
+	/**
+	 * matches [cond] -> [ControllerCalculation] via [map] or [default] if no [map] entry was found
+	 */
+	fun mapOrElse(map: Map<R, ControllerCalculation<T>>, default: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
+		return@ControllerCalculation map[cond.evaluate(accumulation, currentState, target, error, deltaTime)]?.evaluate(accumulation, currentState, target, error, deltaTime) ?: default.evaluate(accumulation, currentState, target, error, deltaTime)
+	}
+
+	/**
+	 * matches [cond] -> [ControllerCalculation] via [map] or returns accumulation (no-op)
+	 *
+	 * evaluates all [ControllerCalculation]s in [map], regardless of which is used.
+	 * this is useful for when a calculation is cheap, but may cause issues if it has a big gap in evaluation,
+	 * e.g. D terms where previous error and delta time may be differently timestamped
+	 *
+	 * if some components need to be forced, and some don't, create two [ControllerCalculation] from this branch, using [map] and [forceMap]
+	 */
+	fun forceMap(map: Map<R, ControllerCalculation<T>>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
+		var res = accumulation
+		val branch = cond.evaluate(accumulation, currentState, target, error, deltaTime)
+		map.forEach { (key, calc) ->
+			val eval = calc.evaluate(accumulation, currentState, target, error, deltaTime)
+			if (key == branch) res = eval
+		}
+		res
+	}
+
+	/**
+	 * matches [cond] -> [ControllerCalculation] via [map] or [default] if no [map] entry was found
+	 *
+	 * evaluates all [ControllerCalculation]s in [map], as well as [default], regardless of which is used.
+	 * this is useful for when a calculation is cheap, but may cause issues if it has a big gap in evaluation,
+	 * e.g. D terms where previous error and delta time may be differently timestamped
+	 *
+	 * if some components need to be forced, and some don't, create two [ControllerCalculation] from this branch, using [map] and [forceMap]
+	 */
+	fun forceMapOrElse(map: Map<R, ControllerCalculation<T>>, default: ControllerCalculation<T>) = ControllerCalculation<T> { accumulation, currentState, target, error, deltaTime ->
+		var res = default.evaluate(accumulation, currentState, target, error, deltaTime)
+		val branch = cond.evaluate(accumulation, currentState, target, error, deltaTime)
+		map.forEach { (key, calc) ->
+			val eval = calc.evaluate(accumulation, currentState, target, error, deltaTime)
+			if (key == branch) res = eval
+		}
+		res
+	}
+
+	/**
+	 * simplified [Branch]
+	 */
 	class Accumulation<T, R>(cond: Function<T, R>) : Branch<T, R>(ControllerComponent { accumulation, _, _, _, _ -> cond.apply(accumulation) })
+	/**
+	 * simplified [Branch]
+	 */
 	class State<T, R>(cond: Function<T, R>) : Branch<T, R>(ControllerComponent { _, currentState, _, _, _ -> cond.apply(currentState) })
+	/**
+	 * simplified [Branch]
+	 */
 	class Target<T, R>(cond: Function<T, R>) : Branch<T, R>(ControllerComponent { _, _, target, _, _ -> cond.apply(target) })
+	/**
+	 * simplified [Branch]
+	 */
 	class Error<T, R>(cond: Function<T, R>) : Branch<T, R>(ControllerComponent { _, _, _, error, _ -> cond.apply(error) })
+	/**
+	 * simplified [Branch]
+	 */
 	class DeltaTime<T, R>(cond: Function<Double, R>) : Branch<T, R>(ControllerComponent { _, _, _, _, deltaTime -> cond.apply(deltaTime) })
 }
