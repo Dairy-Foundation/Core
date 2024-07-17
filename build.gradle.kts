1eb5961fbb761e111d81e56e08cd7c28c70f885e
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-07-17 12:16:35 +1000

    feat(Publication): gradle updated for publication
    see https://repo.dairy.foundation

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,30 +46,41 @@ dependencies {
 	//noinspection GradleDependency
 	implementation("androidx.appcompat:appcompat:1.2.0")
 
-	api(project(":Util"))
 	api(project(":Sinister"))
 
-	compileOnly("org.firstinspires.ftc:RobotCore:9.0.1")
-	compileOnly("org.firstinspires.ftc:Hardware:9.0.1")
-	compileOnly("org.firstinspires.ftc:FtcCommon:9.0.1")
+	compileOnly("org.firstinspires.ftc:RobotCore:9.1.0")
+	compileOnly("org.firstinspires.ftc:Hardware:9.1.0")
+	compileOnly("org.firstinspires.ftc:FtcCommon:9.1.0")
 }
 
 publishing {
+	repositories {
+		maven {
+			name = "Dairy"
+			url = uri("https://repo.dairy.foundation/releases")
+			credentials(PasswordCredentials::class)
+			authentication {
+				create<BasicAuthentication>("basic")
+			}
+		}
+		maven {
+			name = "DairySNAPSHOT"
+			url = uri("https://repo.dairy.foundation/snapshots")
+			credentials(PasswordCredentials::class)
+			authentication {
+				create<BasicAuthentication>("basic")
+			}
+		}
+	}
 	publications {
 		register<MavenPublication>("release") {
 			groupId = "dev.frozenmilk.dairy"
 			artifactId = "Core"
-			version = "v0.0.0"
+			version = "1.0.1"
 
 			afterEvaluate {
 				from(components["release"])
 			}
-		}
-	}
-	repositories {
-		maven {
-			name = "Core"
-			url = uri("${project.buildDir}/release")
 		}
 	}
 }
