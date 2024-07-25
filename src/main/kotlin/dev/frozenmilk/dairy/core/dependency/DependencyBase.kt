9efba01ea2ffded6de57795ab05457810f1bc784
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-07-26 03:56:31 +1000

    fix(Dependency): onFail and onResolve now compose
    Subsequent calls to onFail and onResolve would remove
    previous bindings, this no longer happens.

--- src/main/kotlin/dev/frozenmilk/dairy/core/dependency/DependencyBase.kt
+++ src/main/kotlin/dev/frozenmilk/dairy/core/dependency/DependencyBase.kt
@@ -1,3 +1,10 @@
 package dev.frozenmilk.dairy.core.dependency
 
-open class DependencyBase <T> (dependency: Dependency<T>) : Dependency<T> by dependency
+import dev.frozenmilk.dairy.core.Feature
+import dev.frozenmilk.dairy.core.wrapper.Wrapper
+
+open class DependencyBase <T> (val dependency: Dependency<T>) : Dependency<T> {
+	override fun resolve(opMode: Wrapper, resolvedFeatures: List<Feature>, yielding: Boolean) = dependency.resolve(opMode, resolvedFeatures, yielding)
+	override fun accept(p0: T) = dependency.accept(p0)
+	override fun acceptErr(p0: Throwable) = dependency.acceptErr(p0)
+}
